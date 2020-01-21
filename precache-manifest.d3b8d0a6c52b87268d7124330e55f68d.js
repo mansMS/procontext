9a15dd1575d9b6499bbf13275935c6bab154ded0
mansMS <47422885+mansMS@users.noreply.github.com>
2020-01-22 00:03:46 +0300

    Updates

---
+++ precache-manifest.d3b8d0a6c52b87268d7124330e55f68d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8de2ec12b0ab2a1c6e0b420a433d7c",
+    "url": "/procontext/index.html"
+  },
+  {
+    "revision": "10b201dac7a5d376f9f0",
+    "url": "/procontext/static/css/main.87cf0853.chunk.css"
+  },
+  {
+    "revision": "9375da8c3915a08a64c9",
+    "url": "/procontext/static/js/2.d705bad6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/procontext/static/js/2.d705bad6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "10b201dac7a5d376f9f0",
+    "url": "/procontext/static/js/main.8dfe13ab.chunk.js"
+  },
+  {
+    "revision": "050db60614c77c831070",
+    "url": "/procontext/static/js/runtime-main.d64fba41.js"
+  }
+]);
